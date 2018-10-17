34e34f9762613473d5547cd6fa715d67a316fbdf
lancelot1337 <sudhanshusah2001@gmail.com>
2018-10-17 18:55:48 +0530

    Migrate website

--- app.js
+++ app.js
@@ -4,30 +4,21 @@ const homeRoute = require('./routes/home');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-//for logging
 app.use(morgan('dev'));
-
-//for bodyParser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-//to handle cross-origin resource sharing (CORS) errors
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-
-    //browser send an OPTIONS request when we send post or put request
     if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
         return res.status(200).json({});
     }
     next();
 });
-
-
+app.use(express.static('public'));
 app.use('/', homeRoute);
 
-//for error handling
 app.use((req, res, next) => {
     const error = new Error('Not found');
     error.status = 404;
